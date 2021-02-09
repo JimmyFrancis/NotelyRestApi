c1bd2ffae0e9cde9029c50109dd2814128838cc1
James Francis <james_francis@shaw.ca>
2021-02-09 10:48:15 -0700

    Add controller and swagger

--- NotelyRestApi/Startup.cs
+++ NotelyRestApi/Startup.cs
@@ -5,10 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using NotelyRestApi.Database;
+using NotelyRestApi.Repositories;
+using NotelyRestApi.Repositories.Implementations;
 
 namespace NotelyRestApi
 {
@@ -24,6 +29,13 @@ namespace NotelyRestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Notely Rest API", Version = "v1" });
+            });
+
+            services.AddDbContext<NotelyDbContext>(options => options.UseSqlite(Configuration["Data:NotelyApi:ConnectionString"]));
+            services.AddTransient<INoteRepository, NoteRepository>();
             services.AddControllers();
         }
 
@@ -34,6 +46,12 @@ namespace NotelyRestApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notely Rest API");
+            });
 
             app.UseRouting();
 
